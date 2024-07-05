a26c634f86eccaf45ff892756b0332d6b3cb8b60
Rohan Ahmad <rohanse.1.ra@gmail.com>
2024-07-05 18:02:10 +0500

    vercel adjustment

--- config/tempFileUpload.js
+++ config/tempFileUpload.js
@@ -4,11 +4,7 @@ import {moderateImage} from "../config/picPurifyConfig.js"
 const __dirname = path.resolve();
 
 async function tempFileUpload(buffer){
-    const upload_dir = path.join(process.cwd(),'uploads')
-    if(!fs.existsSync(upload_dir)){
-        fs.mkdirSync(upload_dir)
-    }
-    const tempFilePath = path.join(upload_dir,'tempImage.jpeg')
+    const tempFilePath = path.join('/tmp','tempImage.jpeg')
     await fs.promises.writeFile(tempFilePath,buffer)
     const isSafe = await moderateImage(tempFilePath)
     if(!isSafe){
